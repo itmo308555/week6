3da9aeebb74a4916aae38c34770dedc88540ca61
Ivan Shirokov <ivan.a.shirokov@ya.ru>
2020-11-26 21:11:17 +0600

    week6

--- index.js
+++ index.js
@@ -3,18 +3,15 @@ import fs from 'fs';
 import express from 'express';
 import bodyParser from 'body-parser';
 import m from 'mongoose';
-// import dot from 'dotenv'
 import crypto from 'crypto';
 import http from 'http';
 import CORS from './CORS.js';
 import UserModel from './models/User.js';
-// dot.config({path:'./.env'});
-const {URL} = process.env.MONGO_URI
 const User = UserModel(m);
 const app = appSrc(express, bodyParser, fs, crypto, http, CORS, User);
 const PORT = process.env.PORT || 443;
 try{
-    m.connect(URL, {useNewUrlParser:true, useUnifiedTopology:true});
+    m.connect(process.env.MONGO_URI, {useNewUrlParser:true, useUnifiedTopology:true});
     app.listen(PORT, () => console.log(`Server listening on port ${PORT}!`));
 }
 catch(e){
