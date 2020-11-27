123d76fc54161bee9b6e9a306d370bd2fcfeaa1c
Ivan Shirokov <ivan.a.shirokov@ya.ru>
2020-11-27 13:01:28 +0600

    week6

--- app.js
+++ app.js
@@ -8,21 +8,18 @@ export default function appScr(express, bodyParser, fs, crypto, http, CORS, User
     }
     app
         .use(bodyParser.urlencoded({extended:true}))       
-        .all('/login/', (req, res) => {
-            res.set(headers)
-            res.send('itmo307709');
+        .all('/login/', r => {
+            r.res.set(headers).send('itmo307709');
         })
-        .all('/code/', (req, res) => {
-            res.set(headers)
+        .all('/code/', r => {
+            r.res.set(headers)
             fs.readFile(import.meta.url.substring(7),(err, data) => {
                 if (err) throw err;
-                res.end(data);
+                r.res.end(data);
               });           
         })
-        .all('/sha1/:input/', (req, res) => {
-            res.set(headers)
-            let shasum = crypto.createHash('sha1')
-            res.send(shasum.update(req.params.input).digest('hex'))
+        .all('/sha1/:input/', r => {
+            r.res.set(headers).send(crypto.createHash('sha1').update(r.params.input).digest('hex'))
         })
         .get('/req/', (req, res) =>{
             res.set(headers);
@@ -34,23 +31,23 @@ export default function appScr(express, bodyParser, fs, crypto, http, CORS, User
                 res.send(data)
             })
         })
-        .post('/req/', (req, res) =>{
-            res.set(headers);
+        .post('/req/', r =>{
+            r.res.set(headers);
             const {addr} = req.body;
-            res.send(addr)
+            r.res.send(addr)
         })
-        .post('/insert/', async (req,res)=>{
-            res.set(headers);
-            const {login,password,URL}=req.body;
+        .post('/insert/', async r=>{
+            r.res.set(headers);
+            const {login,password,URL}=r.body;
             const newUser = new User({login,password});
             try{
                 await m.connect(URL, {useNewUrlParser:true, useUnifiedTopology:true});
                 try{
                     await newUser.save();
-                    res.status(201).json({'Добавлено: ':login});
+                    r.res.status(201).json({'Добавлено: ':login});
                 }
                 catch(e){
-                    res.status(400).json({'Ошибка: ':'Нет пароля'});
+                    r.res.status(400).json({'Ошибка: ':'Нет пароля'});
                 }
             }
             catch(e){
